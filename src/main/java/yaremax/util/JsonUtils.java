5e13355e69eb552503b484c1df4aa464c8dd925f
Maksym Yarema <macsym2@gmail.com>
2023-10-22 13:43:45 +0300

    Add Json De/Serialization

---
+++ src/main/java/yaremax/util/JsonUtils.java
@@ -0,0 +1,24 @@
+package yaremax.util;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import yaremax.dto.IDto;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonUtils {
+    private static final Gson GSON = new Gson();
+
+    public static <T extends IDto> List<T> readAllFromJson(String filename, TypeToken<List<T>> typeToken) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        return GSON.fromJson(content, typeToken.getType());
+    }
+
+    public static <T extends IDto> void writeAllToJson(List<T> objects, String filename) throws IOException {
+        String json = GSON.toJson(objects);
+        Files.write(Paths.get(filename), json.getBytes());
+    }
+}
