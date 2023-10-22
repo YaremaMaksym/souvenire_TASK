2653ebf7fb910006aa2cb9ee718bf98ec89f2f3b
Maksym Yarema <macsym2@gmail.com>
2023-10-22 13:45:09 +0300

    Add DuplicateResourceException

---
+++ src/main/java/yaremax/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package yaremax.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
