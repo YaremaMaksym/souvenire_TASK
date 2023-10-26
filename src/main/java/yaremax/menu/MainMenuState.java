cdb02f4ba4098a8c4074fa3d71a09cde19cabfab
Yarema Maksym <macsym2@gmail.com>
2023-10-26 13:08:19 +0300

    completed functionality in MainMenuState

--- src/main/java/yaremax/menu/MainMenuState.java
+++ src/main/java/yaremax/menu/MainMenuState.java
@@ -1,9 +1,12 @@
 package yaremax.menu;
 
+import yaremax.SouvenirFacade;
+
 import java.util.Scanner;
 
 public class MainMenuState implements MenuState {
     private final MenuManager menuManager;
+    private final SouvenirFacade souvenirFacade = new SouvenirFacade();
 
     public MainMenuState(MenuManager menuManager) {
         this.menuManager = menuManager;
@@ -15,19 +18,55 @@ public class MainMenuState implements MenuState {
                 \n
                 Головне меню:
                 1️⃣ - Меню виробників та сувенірів
-                2️⃣ - 
-                3️⃣ - 
-                4️⃣ - Exit
+                2️⃣ - Переглянути сувеніри певного виробника
+                3️⃣ - Переглянути сувеніри, виготовлені в певній країні
+                4️⃣ - Переглянути виробників, чиї ціни менше заданої
+                5️⃣ - Переглянути сувеніри за виробниками
+                6️⃣ - Переглянути виробників за сувеніром(ім'я, рік)
+                7️⃣ - Переглянути сувеніри за роками
+                8️⃣ - Видалити заданого виробника та його сувеніри.
+                9️⃣ - Завершити роботу
                 """);
     }
 
     @Override
     public void handleInput(String input) {
+        Scanner in = new Scanner(System.in);
+
         switch (Integer.parseInt(input)) {
             case 1 -> menuManager.setCurrentState(menuManager.getProducerSouvenirMenuState());
-            case 2 -> {}
-            case 3 -> {}
-            case 4 -> menuManager.setCurrentState(menuManager.getExitState());
+            case 2 -> {
+                System.out.print("Enter producer name: ");
+                String producerName = in.nextLine();
+                souvenirFacade.viewSouvenirsByProducer(producerName);
+            }
+            case 3 -> {
+                System.out.print("Enter country: ");
+                String country = in.nextLine();
+                souvenirFacade.viewSouvenirsByCountry(country);
+            }
+            case 4 -> {
+                System.out.print("Enter price limit: ");
+                // todo: handle ex.
+                double priceLimit = Double.parseDouble(in.nextLine());
+                souvenirFacade.viewProducersByPriceLimit(priceLimit);
+            }
+            case 5 -> souvenirFacade.viewSouvenirsByProducers();
+            case 6 -> {
+                System.out.print("Enter souvenir name: ");
+                String souvenirName = in.nextLine();
+                System.out.print("Enter souvenir year: ");
+                // todo: handle ex.
+                int year = Integer.parseInt(in.nextLine());
+                souvenirFacade.viewProducersBySouvenir(souvenirName, year);
+            }
+            case 7 -> souvenirFacade.viewSouvenirsByYears();
+            case 8 -> {
+                System.out.print("Enter producer name: ");
+                String producerName = in.nextLine();
+                souvenirFacade.deleteProducerAndSouvenirs(producerName);
+            }
+            case 9 -> menuManager.setCurrentState(menuManager.getExitState());
             default -> System.out.println(input + " not a valid option");
         }
     }
